583992a7f0bb2632b44c4c642d552f3215d87add
SB604-09 <SB604-09@DESKTOP-9EJDQ7R>
2019-12-02 13:50:57 +0700

    hitung complite

--- src/main/java/id/ac/unpas/kakas/hitung.java
+++ src/main/java/id/ac/unpas/kakas/hitung.java
@@ -21,4 +21,16 @@ public class hitung {
 	        return hasil;
 	    }
 	
+	public void int kali(int n1, int n2) {
+		
+        int hasil = n1*n2;
+        return hasil;
+	}
+	
+	public void int bagi(int n1, int n2) {
+		
+        int hasil = n1/n2;
+        return hasil;
+	}
+	
 }
