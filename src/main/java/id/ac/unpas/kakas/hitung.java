63c6a5c946d956de468ba444e1f280aa134816b3
SB604-09 <SB604-09@DESKTOP-9EJDQ7R>
2019-12-02 13:48:08 +0700

    main

--- src/main/java/id/ac/unpas/kakas/hitung.java
+++ src/main/java/id/ac/unpas/kakas/hitung.java
@@ -5,23 +5,17 @@ import java.util.Scanner;
 public class hitung {
 	
 	
-	public static int tambah()
+	public static int tambah(int n1, int n2)
 	    {
-			Scanner sc = new Scanner(System.in);
 
-			int n1 =  sc.nextInt();
-			int n2 =  sc.nextInt();
 			
 	        int hasil = n1 + n2;
 	        return hasil;
 	    }
 	  
-	public static int kurang()
+	public static int kurang(int n1 ,int n2)
 	    {
-			Scanner sc = new Scanner(System.in);
-	
-			int n1 =  sc.nextInt();
-			int n2 =  sc.nextInt();
+
 			
 	        int hasil = n1-n2;
 	        return hasil;
--- src/main/java/id/ac/unpas/kakas/main.java
+++ src/main/java/id/ac/unpas/kakas/main.java
@@ -1,7 +1,31 @@
 package id.ac.unpas.kakas;
 
+import java.util.Scanner;
+
 public class main {
 	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pilih Menu");
+		int menu = 0;
+		menu = sc.nextInt();
+		int n1 , n2 ;
+		int total = 0;
+		System.out.println("Masukan Variabel 1");
+		n1 = sc.nextInt();
+		System.out.println("Masukan Variabel 1");
+		n2 = sc.nextInt();
+		hitung hi = new hitung();
+		switch (menu) {
+		case 1:
+				total = hi.tambah(n1, n2);
+				System.out.println(total);
+			break;
 
+		default:
+			break;
+		}
+
+	}
 	
 }
